eaff95fb99a85b772e4f142cf92635893808f026
Дмитрий Васильев <dmitriy.vas.b@gmail.com>
2019-03-26 04:37:13 +0500

    normalize is unexported now

--- Go/Dmitriy-Vas/Check Anagram/anagram.go
+++ Go/Dmitriy-Vas/Check Anagram/anagram.go
@@ -10,10 +10,10 @@ func Anagram(s string, w string) bool {
 	r := regexp.MustCompile("[^a-z]")
 	s = r.ReplaceAllString(strings.ToLower(s), "")
 	w = r.ReplaceAllString(strings.ToLower(w), "")
-	return Normalize(w) == Normalize(s)
+	return normalize(w) == normalize(s)
 }
 
-func Normalize(s string) string {
+func normalize(s string) string {
 	runes := strings.Split(s, "")
 	sort.Strings(runes)
 	return strings.Join(runes, "")
