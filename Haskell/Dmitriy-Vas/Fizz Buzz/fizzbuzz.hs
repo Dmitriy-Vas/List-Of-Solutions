fd4e692c07e24737bf477866575d99052b4e20da
Дмитрий <Dmitriy.Vas.B@gmail.com>
2019-06-04 20:29:03 +0500

    Update "FizzBuzz" package: add "where" clauses

--- Haskell/Dmitriy-Vas/Fizz Buzz/fizzbuzz.hs
+++ Haskell/Dmitriy-Vas/Fizz Buzz/fizzbuzz.hs
@@ -1,16 +1,10 @@
 module Fizz_Buzz where
 
 fizzBuzz :: Int -> String
-fizzBuzz a | mod15 a = "FizzBuzz"
-           | mod3 a = "Fizz"
-           | mod5 a = "Buzz"
+fizzBuzz a | mod15 = "FizzBuzz"
+           | mod3 = "Fizz"
+           | mod5 = "Buzz"
            | otherwise = show a
-
-mod3 :: Int -> Bool
-mod3 a = mod a 3 == 0
-
-mod5 :: Int -> Bool
-mod5 a = mod a 5 == 0
-
-mod15 :: Int -> Bool
-mod15 a = mod a 15 == 0
+  where mod3 = mod a 3 == 0
+        mod5 = mod a 5 == 0
+        mod15 = mod a 15 == 0
